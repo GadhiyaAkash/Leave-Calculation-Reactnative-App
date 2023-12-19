7bec0cd6769aac21de0149ff1e7180ead7b957a4
Akash Gadhiya <akashpatel1101@gmail.com>
2023-12-19 11:03:48 +0530

    Fixed expo cli warning

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text></Text>
+      <Text>Welcome Dear</Text>
       <StatusBar style="auto" />
     </View>
   );
